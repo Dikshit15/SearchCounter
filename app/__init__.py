4054df6a7ce91c35207a99d255f0af8a753ee133
Aniket Agarwal <aniket@localhost.localdomain>
2019-11-20 10:20:06 +0530

    heroku client

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/tapsearch"
-mongo = PyMongo(app)
-from . import routes
--- tapsearch.py
+++ tapsearch.py
@@ -1,1 +1,8 @@
-from app import app
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tapsearch"
+mongo = PyMongo(app)
+from .app import routes
