dd55abfe00ed06bb0866fb6622001d1449b716f1
Aniket Agarwal <aniket@localhost.localdomain>
2019-11-20 13:22:28 +0530

    adding user

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/tapsearch"
+app.config["MONGO_URI"] = "mongodb+srv://tapsearch:lhgZh0HJ0xqbTY6w@cluster0-i7yjg.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 from . import routes
